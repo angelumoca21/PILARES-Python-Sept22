1d04c275f8002aee5a58ee87753893ed7d900df2
ANGEL URIEL MORALES CARDOSO <angelmoca21@gmail.com>
2022-10-07 18:04:03 -0500

    Create s3.py

---
+++ s3.py
@@ -0,0 +1,24 @@
+#Modulos
+#Defincion de funciones
+#Definicion/incializacion de variables
+#Instrucciones
+
+edad = 50 #Creacion y asignacion de variable entera
+promedio = 9.65 #Creacion de variable flotante
+estatusEstudiante = True #Creacion de variable boolena
+nombre = "uriel" #Creacion de variable tipo string
+
+edad2 = 14 #Creacion y asignacion de variable entera
+promedio2 = 6.65 #Creacion de variable flotante
+estatusEstudiante2 = True #Creacion de variable boolena
+nombre2 = "Xochitl" #Creacion de variable tipo string
+
+print("Hola a  todos en taller PYTHON")
+print('Hola a  todos en taller Python')
+print(type(edad)) #Imprimir tipo de variable que es edad
+print(type(promedio)) #Imprimir tipo de variable que es promedio
+print(type(estatusEstudiante))#Imprimir tipo de variable que es estatusEstudiante
+print(type(nombre))#Imprimir tipo de variable que es nombre
+print("El estudiante Uriel de 25 años tiene un promedio de 5.65 y tiene un estatus de False")
+print("El estudiante " + nombre + " de " + str(edad) + " años tiene un promedio de " + str(promedio) + " y tiene un estatus de " + str(estatusEstudiante)) #Impresion de enunciado completo imprimiendo valores de las variablesssssss
+print("El estudiante " + nombre2 + " de " + str(edad2) + " años tiene un promedio de " + str(promedio2) + " y tiene un estatus de " + str(estatusEstudiante2))
