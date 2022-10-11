70f855535fcc763b1c16049c745187c0b979f124
angelumoca21 <angelmoca21@gmail.com>
2022-10-11 09:26:01 -0500

    Create retoS3.py

---
+++ retoS3.py
@@ -0,0 +1,37 @@
+print(
+"""
+         _nnnn_
+        dGGGGMMb
+       @p~qp~~qMb
+       M|@||@) M|
+       @,----.JM|
+      JS^\__/  qKL
+     dZP        qKRb
+    dZP          qKKb
+   fZP            SMMb
+   HZM            MMMM
+   FqM            MMMM
+ __| ".        |\dS"qML
+ |    `.       | `' \Zq
+_)      \.___.,|     .'
+\____   )MMMMMP|   .'
+     `-'       `--' hjm
+"""
+)
+
+print("         _nnnn_")
+print("        dGGGGMMb")
+print("       @p~qp~~qMb")
+print("       M|@||@) M|")
+print("       @,----.JM|")
+print("      JS^\__/  qKL")
+print("     dZP        qKRb")
+print("    dZP          qKKb")
+print("   fZP            SMMb")
+print("   HZM            MMMM")
+print("   FqM            MMMM")
+print(' __| ".        |\dS"qML')
+print(" |    `.       | `' \Zq")
+print("_)      \.___.,|     .'")
+print("\____   )MMMMMP|   .'")
+print("     `-'       `--' hjm")
