65b09eddb49280ecea94cd9ac3dfa3dd8ad5b917
angelumoca21 <angelmoca21@gmail.com>
2022-10-14 17:17:19 -0500

    Create s4.py

---
+++ s4.py
@@ -0,0 +1,20 @@
+"""edad = input("¿Cual es tu edad?") #Solicitar datos al usuario (teclado).
+print(type(edad)) #Que tipo de variable es edad
+print("Gracias, tu edad es de " + edad + " años.") #Imprimir edad.
+
+edad = input("¿Cual es tu edad?")
+print(type(edad))
+edad = int(edad) + 5 #Se convierte edad a entero para poder manejarlo matematicamente.
+print(type(edad))
+print("Gracias, tu edad es de " + str(edad) + " años.")
+
+precio = input("Ingresa el precio del producto")
+print(type(precio))#str
+precio = float(precio)
+print(type(precio))#float
+
+status = input("Ingresa tu status escolar: [True, False]")
+print(type(status))#str
+status = bool(status)
+print(type(status))#bool"""
+
