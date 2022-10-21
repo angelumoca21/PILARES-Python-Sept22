ca676fdd902d440625ebeb5c13a2ee9739ee343c
angelumoca21 <angelmoca21@gmail.com>
2022-10-21 17:54:47 -0500

    Create s5.py

---
+++ s5.py
@@ -0,0 +1,16 @@
+resultado = 3**2 + 7*((2-1)**5)
+edad = 26 #variable aux para ejemplo
+print(f"El resultado de la operacion es {resultado} y mi edad es {edad}")
+print("El resultado de la operacion es " + str(resultado))
+
+"""
+AND->Estricto
+Condiciones NECESARIAS PARA LA aprobacion de materia:
+a)Entregar todas las tareas(1) /// No entregar tareas (0)
+b)Pasar examen(1) /// No pasar examen (0)
+
+OR->RELAX
+Condiciones MINIMAS PARA APROBAR:
+a)Entregar todas las tareas(1) /// No entregar tareas (0)
+b)Pasar examen(1) /// No pasar examen (0)
+"""
