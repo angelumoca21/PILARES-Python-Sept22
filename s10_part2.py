70ac5610e616d61f14fb2c05361f424abd00683c
angelumoca21 <angelmoca21@gmail.com>
2022-11-26 08:51:24 -0600

    Update s10_part2.py

--- s10_part2.py
+++ s10_part2.py
@@ -4,12 +4,16 @@
 3.Llenar las lista calificaciones
 4.Imprimir elementos
 """
-asignarutas = ["Mate","Fisica","Quimica","Historia","Lengua"]
+asignaturas = ["Mate","Fisica","Quimica","Historia","Lengua"]
 calificaciones = []
 
-for asignatura in asignarutas:
+for asignatura in asignaturas:
     cal = input(f"Cuanto sacaste en {asignatura}:")
     calificaciones.append(cal)
 
-for asignatura,calificacion in zip(asignarutas,calificaciones):
-    print(f"En la asignatura {asignatura} has sacado {calificacion}")
+"""for asignatura,calificacion in zip(asignarutas,calificaciones):
+    print(f"En la asignatura {asignatura} has sacado {calificacion}")"""
+
+tam = len(calificaciones)
+for i in range(0,tam):
+    print("En la asignatura " + asignaturas[i] + " has sacado " + calificaciones[i])
